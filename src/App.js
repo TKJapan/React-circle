45838412516e08e18235c9d4440857f6d83b4bbe
tgithubk <pianizm@hotmail.com>
2022-05-22 10:59:36 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="center">
+        <div className="circle"><p className="text">React</p></div>
+        <div className="circle"><p className="text">Vue</p></div>
+        <div className="circle"><p className="text">Django</p></div>
+      </div>
     </div>
   );
 }
